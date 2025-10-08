31d639540e1a1df439ae9b4eaae04958e10c1476
Gabriel <ghaabriieel@gmail.com>
2025-10-07 21:08:03 -0300

    Adcionando arquivo .env para abstrair dados importantes do projeto

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,14 +1,17 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
 import { User } from "./entity/User"
+import dotenv from "dotenv"
+dotenv.config()
+
 
 export const AppDataSource = new DataSource({
-    type: "mysql",
-    host: "localhost",
-    port: 3306,
-    username: "test",
-    password: "test",
-    database: "test",
+    type: process.env.DB_TYPE as any,
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     synchronize: true,
     logging: false,
     entities: [User],
