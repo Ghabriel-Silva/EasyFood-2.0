6ccf33f5bace98867413a6730e9d147d9ee87138
Gabriel <ghaabriieel@gmail.com>
2025-10-12 20:10:31 -0300

    Adcionando criptografia de senhas na hora de registrar user

--- src/app/service/UserService/Auth.ts
+++ src/app/service/UserService/Auth.ts
@@ -3,6 +3,8 @@ import { UserRepository } from "../../repository/UserRepository/Auth";
 import ErrorExtension from "../../utils/ErrorExtension";
 import registerSchema from "../../validations/AuthUser/Register";
 import * as yup from "yup";
+import bcrypt from "bcrypt";
+
 
 export class UserAuthService {
     private userRepository: UserRepository
@@ -12,7 +14,7 @@ export class UserAuthService {
     }
 
     register = async (register: IRegister) => {
-      
+
         try {
             await registerSchema.validate(register, { abortEarly: false })
         } catch (err) {
@@ -22,13 +24,17 @@ export class UserAuthService {
             throw err
         }
 
+        const hashPassword:string = await bcrypt.hash(register.password, 10)
+
+        register.password = hashPassword
+
         const validEmail: IRegister | null = await this.userRepository.findByEmail(register.email)
         if (validEmail) {
             throw new ErrorExtension(401, 'Email já cadastrado!')
         }
         const registrandoUser = await this.userRepository.create(register)
-        
-        if(!registrandoUser){
+
+        if (!registrandoUser) {
             throw new ErrorExtension(400, 'Usuário não resgitrado')
         }
 
