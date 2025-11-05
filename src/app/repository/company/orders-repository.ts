26e3e4dcdf6233dfbc09d915f0d00f60e597f916
Gabriel <ghaabriieel@gmail.com>
2025-11-05 12:31:11 -0300

    feat(order): define lógica de negócio para cálculo do frete

--- src/app/repository/company/orders-repository.ts
+++ src/app/repository/company/orders-repository.ts
@@ -4,6 +4,7 @@ import { Order } from "../../entity/Order"
 import { CreateOrderSchema } from "../../validations/company/order/create"
 import { myJwtPayload } from "../../interfaces/i-auth/i-auth"
 import { OrderItem } from "../../entity/OrderItem"
+import { Company } from "../../entity/Company"
 
 
 
@@ -15,16 +16,18 @@ class orderRepository {
         this.orderRepo = AppDataSource.getRepository(Order)
     }
 
-    async createOrder(data: CreateOrderSchema, company: myJwtPayload): Promise<Order | null> {
+    async createOrder(data: CreateOrderSchema, company: myJwtPayload, sumFreight: number): Promise<Order | null> {
         const newOrder: Order = await this.orderRepo.create({
             ...data as DeepPartial<Order>,
+            totalFreight: sumFreight,
             company: { id: company.id },
         })
+        console.log(newOrder)
         await this.orderRepo.save(newOrder)
 
         const items = data.items.map(item => ({
             quantity: item.quantity,
-            subtotal:item.quantity * item.price,
+            subtotal: item.quantity * item.price,
             price: item.price,
             order: { id: newOrder.id },
             product: { id: item.product_id }
@@ -38,13 +41,21 @@ class orderRepository {
             .execute()
 
         const orderWithItems = await this.orderRepo.findOne({
-            where: {id: newOrder.id}, 
-            relations:["items", "items.product"]
+            where: { id: newOrder.id },
+            relations: ["items", "items.product"]
         })
-        console.log(JSON.stringify(orderWithItems, null, 2))
         return orderWithItems
     }
 
+    async getCompanyFreightValue(company: myJwtPayload): Promise<Company | null> {
+        return await AppDataSource
+            .getRepository(Company)
+            .createQueryBuilder('company')
+            .select(['company.defaultFreight'])
+            .where("company.id = :id", { id: company.id })
+            .getOne()
+
+    }
 
 }
 
--- src/app/service/company/orders-service.ts
+++ src/app/service/company/orders-service.ts
@@ -5,6 +5,7 @@ import * as yup from "yup";
 import ErrorExtension from "../../utils/error-extension";
 import { createOrderSchema, CreateOrderSchema } from "../../validations/company/order/create";
 import { Order } from "../../entity/Order";
+import { Company } from "../../entity/Company";
 
 
 
@@ -17,11 +18,33 @@ class orderService {
 
     createOrder = async (data: IOrdersRegister, payloudCompany: myJwtPayload) => {
         try {
+            let freteDefault: number | undefined
             const validateOrder: CreateOrderSchema = await createOrderSchema.validate(data, {
                 abortEarly: false
             })
-            const dataOrder:Order | null = await this.orderRepository.createOrder(validateOrder, payloudCompany)
-            if(!dataOrder){
+
+            //Pega o valor padrão de frete que a empresa definio em Confg
+            if (validateOrder.isFreightApplied) {
+                const freteCompany: Company | null = await this.orderRepository.getCompanyFreightValue(payloudCompany)
+                if (!freteCompany) {
+                    throw new ErrorExtension(404, 'Valor do frete da compania não encontrado')
+                }
+                freteDefault = freteCompany.defaultFreight
+            }
+
+            //Se a empresa definir um valor customizado  para o pedido ira pegar esse valor 
+            const customFreight: number =
+                typeof validateOrder.customFreight === 'number'
+                    ? validateOrder.customFreight
+                    : 0
+
+            //Soma valor de frete Default e Custon  se caso o frete da empresa vier como undefine define 0 
+            const sumFreight: number =
+                (freteDefault ?? 0) + (customFreight ?? 0)
+
+
+            const dataOrder: Order | null = await this.orderRepository.createOrder(validateOrder, payloudCompany, sumFreight)
+            if (!dataOrder) {
                 throw new ErrorExtension(400, 'Erro ao salvar Pedido')
             }
             return dataOrder
@@ -30,6 +53,7 @@ class orderService {
             if (err instanceof yup.ValidationError) {
                 throw new ErrorExtension(400, err.errors.join(","))
             }
+            throw err;
         }
     }
 
